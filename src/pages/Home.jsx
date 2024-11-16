44dfffa34de6a1f0731a91db0d03ec320bbd1198
pauloesmelos <pauloempdel@hotmail.com>
2024-11-16 00:53:37 -0300

    fixing error vercel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navbar from '../components/Home/Navbar';
 import About from '../components/home/About';
 import Blog from '../components/home/Blog';
 import Features from '../components/home/Features';
 import Footer from '../components/home/Footer';
 import Hero from '../components/home/Hero';
+import Navbar from '../components/home/Navbar';
 import Newsletter from '../components/home/Newsletter';
 import Plans from '../components/home/Plans';
 
